940b6ad95b9a22c31c48aee6d282b5af6634ea1d
Kiara Johnson <kiarajohnson91@gmail.com>
2023-08-17 11:01:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//parent class class Shape 
+
+//child class class Triangle
+
+//child class class Circle
+
+//child class class Square
+
+// const shape = new Triangle();
+// shape.setColor("blue");
+// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
